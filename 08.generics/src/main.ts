c561f11e6384faa3fc9218dfc03e554b38497e71
June <sungj1207@gmail.com>
2023-07-24 18:19:31 +0900

    Generic Levelup

--- 08.generics/src/main.ts
+++ 08.generics/src/main.ts
@@ -8,36 +8,66 @@
 
 // [Generic 사용]
 
-function getArrayLength<T>(arr: T[]):number {
-  return arr.length; 
+// function getArrayLength<T>(arr: T[]):number {
+//   return arr.length; 
+// }
+
+// const array1 = [1, 2, 3];
+// const array2 = ["a", "b", "c"];
+// const array3 = [true, false, true];
+
+// getArrayLength<number>(array1);
+// getArrayLength<string>(array2);
+// getArrayLength<boolean>(array3);
+
+
+// // Generic을 사용해서 any 처리하기
+// interface Vehicle<T> {
+//   name: string;
+//   color: string;
+//   option: T; //any;
+// }
+
+// const car: Vehicle<{ price: number }> = {
+//   name: 'Car',
+//   color: 'red',
+//   option: {
+//     price: 1000
+//   }
+// }
+
+// const bike: Vehicle<boolean> = {
+//   name: 'Bike',
+//   color: 'green',
+//   option: true
+// }
+
+// ----
+
+// [Generic Level Up]
+
+const makeArr = <X, Y> (x: X, y: Y): [X, Y] => {
+  return [x, y];
 }
 
-const array1 = [1, 2, 3];
-const array2 = ["a", "b", "c"];
-const array3 = [true, false, true];
+const array = makeArr<number, number>(4, 5);
+const array2 = makeArr<string, string>("a", "b");
 
-getArrayLength<number>(array1);
-getArrayLength<string>(array2);
-getArrayLength<boolean>(array3);
-
-
-// Generic을 사용해서 any 처리하기
-interface Vehicle<T> {
-  name: string;
-  color: string;
-  option: T; //any;
+// 기본값 넣기
+const makeArr1 = <X, Y = string> (x: X, y: Y): [X, Y] => {
+  return [x, y];
 }
 
-const car: Vehicle<{ price: number }> = {
-  name: 'Car',
-  color: 'red',
-  option: {
-    price: 1000
+const array1 = makeArr1<string>("a", "b");
+
+
+// firstName + lastName + 이외의 값
+const makeFullName = <T extends { firstName: string, lastName: string }>(obj: T) => {
+  return {
+    ...obj,
+    fullName: obj.firstName + " " + obj.lastName
   }
 }
-
-const bike: Vehicle<boolean> = {
-  name: 'Bike',
-  color: 'green',
-  option: true
-}
+// 호출
+// firstName + lastName 속성은 무조건 있어야 한다.
+makeFullName({ firstName: "John", lastName: "Kim", location: 'Seoul' })
