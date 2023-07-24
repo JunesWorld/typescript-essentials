2f83c09de4d3f098962dfa9f9ef0936be834d18f
June <sungj1207@gmail.com>
2023-07-24 17:09:18 +0900

    Generic 기본

---
+++ 08.generics/src/main.ts
@@ -0,0 +1,43 @@
+// (arr: number[]) = 매개변수 arr 타입은 Arr의 number
+// :number = number로 반환
+// function getArrayLength(arr: number[] | string[] | boolean[]):number {
+//   return arr.length; 
+// }
+
+// ----
+
+// [Generic 사용]
+
+function getArrayLength<T>(arr: T[]):number {
+  return arr.length; 
+}
+
+const array1 = [1, 2, 3];
+const array2 = ["a", "b", "c"];
+const array3 = [true, false, true];
+
+getArrayLength<number>(array1);
+getArrayLength<string>(array2);
+getArrayLength<boolean>(array3);
+
+
+// Generic을 사용해서 any 처리하기
+interface Vehicle<T> {
+  name: string;
+  color: string;
+  option: T; //any;
+}
+
+const car: Vehicle<{ price: number }> = {
+  name: 'Car',
+  color: 'red',
+  option: {
+    price: 1000
+  }
+}
+
+const bike: Vehicle<boolean> = {
+  name: 'Bike',
+  color: 'green',
+  option: true
+}
